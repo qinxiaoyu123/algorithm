3aa3d46c4e2741b1fac5323dbc1eb20b9fb6e3f7
qinxiaoyu123 <1041411393@qq.com>
2020-02-01 22:47:39 +0800

    DigitalTriangle

---
+++ src/main/java/dynamicProgramming/DigitalTriangle.java
@@ -0,0 +1,45 @@
+package dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DigitalTriangle {
+    //    static int idx = 0;
+    static int N = 510;
+    static int INF = -0x3f3f3f3f;
+    static int[][] g = new int[N][N];
+    static int[][] f = new int[N][N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 1; i <= n; i++) {
+            int idx = 1;
+            for (String s : reader.readLine().split("\\s+")) {
+                g[i][idx++] = Integer.parseInt(s);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                f[i][j] = INF;
+            }
+        }
+        f[1][1] = g[1][1];i
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= ; j++) {
+                if (1 <= j && j <= i) {
+                    f[i][j] = Math.max(f[i - 1][j] + g[i][j], f[i][j]);
+                }
+                if (1 <= j - 1 && j - 1 <= i) {
+                    f[i][j] = Math.max(f[i - 1][j - 1] + g[i][j], f[i][j]);
+                }
+            }
+        }
+        int res = INF;
+        for (int i = 1; i <= n; i++) {
+            res = Math.max(f[n][i], res);
+        }
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/dynamicProgramming/DigitalTriangle1.java
@@ -0,0 +1,40 @@
+package dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//不加j的判断版本
+public class DigitalTriangle1 {
+    static int N = 510;
+    static int INF = -0x3f3f3f3f;
+    static int[][] g = new int[N][N];
+    static int[][] f = new int[N][N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 1; i <= n; i++) {
+            int idx = 1;
+            for (String s : reader.readLine().split("\\s+")) {
+                g[i][idx++] = Integer.parseInt(s);
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n+1; j++) {
+                f[i][j] = INF;
+            }
+        }
+        f[1][1] = g[1][1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                f[i][j] = Math.max(f[i - 1][j] + g[i][j], f[i - 1][j - 1] + g[i][j]);
+            }
+        }
+        int res = INF;
+        for (int i = 1; i <= n; i++) {
+            res = Math.max(f[n][i], res);
+        }
+        System.out.println(res);
+    }
+}
