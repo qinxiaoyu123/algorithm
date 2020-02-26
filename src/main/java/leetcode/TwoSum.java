14e61568af5db1ccba646bb0053979ab401e9d55
qinxiaoyu123 <1041411393@qq.com>
2020-02-26 22:26:09 +0800

    leetcode 1

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        //数组不能排序
         for(int i = 0; i<nums.length;i++){
             for(int j = i+1; j<nums.length;j++){
                 if(nums[i]+nums[j] == target){
